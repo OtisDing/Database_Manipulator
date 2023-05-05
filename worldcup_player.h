0d2b5cd334dec18b7f6d2b9d912351552c31dfb1
OtisDing <93888172+OtisDing@users.noreply.github.com>
2023-05-05 14:36:11 -0400

    Add files via upload

---
+++ worldcup_player.h
@@ -0,0 +1,15 @@
+//Header file for worldCup_player
+//Here we have all our definition, and our function prototypes
+
+#define NAME_LEN 49
+#define CLUB_LEN 49
+
+char *gets(char *str);
+
+void insert();
+void search();
+void update();
+void print();
+void delete();
+void read_line(char str[], int n);
+void player();
